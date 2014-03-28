20601ffcc3c1e6c3fcff385da0c7fa7a9f2cdf44
Albert Wu <albertyfwu@gmail.com>
2014-03-28 15:11:01 -0400

    save time on multinomial

--- 452.py
+++ 452.py
@@ -19,19 +19,18 @@ def initialize_factorials():
 	for i in range(0, 30):
 		fact_cache.append(math.factorial(i))
 
-def multi(top, bottoms):
-	# biggest number will be the first one
-	ret = 1
-	largest = bottoms[0]
-	for i in range(largest + 1, top + 1):
-		ret *= i
-	for i in range(1, len(bottoms)):
-		ret /= fact(bottoms[i])
-	return ret
+def binomial(n, k):
+	ntok = 1
+	ktok = 1
+	for t in xrange(1, min(k, n - k) + 1):
+		ntok *= n
+		ktok *= t
+		n -= 1
+	return ntok / ktok
 
 count = 0
 
-def num_ways(used, used_prod, used_sum, arg, m):
+def num_ways(used, used_prod, used_sum, used_multi, arg, m):
 	global count 
 	start_t = time.time()
 	n = len(used)
@@ -40,10 +39,9 @@ def num_ways(used, used_prod, used_sum, arg, m):
 	rem = arg - used_sum
 
 	if rem == 0:
-		ret = multi(arg, used) % m
-		# ret = 1
-		# print used, ret
-		# return 1
+		ret = used_multi
+		end_t = time.time()
+		count += end_t - start_t
 		return ret
 
 	# now we want to see what's the least number of n+1 we need in here so that n+2 doesn't exceed arg
@@ -51,17 +49,15 @@ def num_ways(used, used_prod, used_sum, arg, m):
 	# => p * (n+2)^rem * (n+1)^x / (n+2)^x <= arg
 	# => [(n+1)/(n+2)]^x <= arg / [p * (n+2)^rem]
 	# => x = ceil[log(arg / [p * (n+2)^rem], (n+1)/(n+2))]
-	# start_t = time.time()
 	min_n1 = max(int(math.ceil(math.log(arg / (p * float(n+2)**rem), (n+1) / float(n+2)))), 0)
-	end_t = time.time()
-	count += end_t - start_t
-	ret = sum([num_ways(used + [i], used_prod * (n + 1)**i, used_sum + i, arg, m) for i in range(min_n1, arg - used_sum + 1)])
+	ret = sum([num_ways(used + [i], used_prod * (n + 1)**i, used_sum + i, (used_multi * binomial(used_sum + i, i)) % m, arg, m) 
+		for i in range(min_n1, arg - used_sum + 1)])
 	return ret
 
 start = time.time()
 
 initialize_factorials()
-print num_ways([], 1, 0, ARG, MOD)
+print num_ways([], 1, 0, 1, ARG, MOD)
 
 end = time.time()
 print end - start, count, count / (end - start)
